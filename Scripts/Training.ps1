b55357105c13e4ddc971a0e0b05ad2a1552cdfc0
Nahuel Desimone <nahuel.desimone98@gmail.com>
2021-05-07 12:28:33 -0300

    Se agregan al script lo visto en el curso acerca de condicionales

--- Scripts/Training.ps1
+++ Scripts/Training.ps1
@@ -26,6 +26,64 @@ Write-Host @resultado
 $persona -contains "Nahuel" #Devuelve True o False si un elemnto esta contenido en un array
 #-notcontains es lo mismo pero a la inversa.
 
+#Condicionales
+#Si tengo una varias condiciones se utiliza el elseif
+
+
+[int]$edad = Read-Host "Ingrese su edad"
+
+if ($edad -ge 18) {
+    Write-Host "Es mayor de edad"
+}
+elseif (($edad -lt 18) -And ($edad -ge 13)) {
+    Write-Host "Es adolescente"
+}
+else {
+    Write-Host "Es muy chiquito"
+}
+
+#Cuando tenemos más de 2 casos disponibles se usa el switch
+
+switch ($edad) {
+    {$edad -ge 18  }{
+        Write-Host "Es mayor de edad"
+        Break
+    }
+    {$edad -lt 18 -and $edad -gt 13}{
+        Write-Host "Es adolescente"
+        Break
+    }
+    Default {
+        Write-Host "Es muy pequeño"
+        Break
+    }
+}
+
+$color = Read-Host "Elija un color"
+
+switch ($color) {
+    "rojo"{
+        Write-Host "No puede pasar"
+        Break
+    } 
+    "amarillo" {
+        Write-Host "Espere unos segundos"
+        Break
+    }
+    "verde"{ 
+        Write-Host "Puede pasar"
+        Break
+    }
+    default {
+        Write-Host "Color no valido"
+    }
+}
+
+
+
+
+
+
 
 
 
