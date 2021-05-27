e354d6bdd0cbffa7763a28d9b225d2f35d037649
Nahuel Desimone <nahuel.desimone98@gmail.com>
2021-05-27 12:09:43 -0300

    Se agrega un script para automatizar el cambio de contraseña de los usuarios

---
+++ Scripts/Curso Udemy/ResetPassUsers.ps1
@@ -0,0 +1,3 @@
+﻿$Cuenta = (Read-Host -Prompt "Ingrese nombre de usuario")
+$NewPassword = (Read-Host -Prompt "Ingrese nueva contraseña" -AsSecureString)
+Set-ADAccountPassword -Identity $Cuenta -NewPassword $NewPassword -Reset
