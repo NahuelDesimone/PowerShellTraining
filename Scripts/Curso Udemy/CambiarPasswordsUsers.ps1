7bf885bf547306e459fdf548d85f692887ce445f
Nahuel Desimone <nahuel.desimone98@gmail.com>
2021-05-27 11:58:46 -0300

    Se agrega script para restablecer contraseñas de usuarios

---
+++ Scripts/Curso Udemy/CambiarPasswordsUsers.ps1
@@ -0,0 +1,6 @@
+﻿#Cambiar contraseña
+Set-ADAccountPassword -Identity jgomez
+
+Set-ADAccountPassword -Identity jgomez -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "P@ssword" -Force)
+
+Get-ADUser -Filter * -SearchBase "OU=Usuarios,DC=CONTOSO,DC=local" | Set-ADAccountPassword -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "P@ssword" -Force)
