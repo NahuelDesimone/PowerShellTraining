85c8b8c1b4ba370a7edf11a4ee16f82352dd4974
Nahuel Desimone <nahuel.desimone98@gmail.com>
2021-06-03 12:09:58 -0300

    Se crea script para cambiarle las horas de inicio permitidas a un usuario

---
+++ Scripts/Curso Udemy/Asignar horas de inicio de sesión a usuarios.ps1
@@ -0,0 +1,8 @@
+﻿#Asignar a usuario
+[byte[]]$horario=@(000,000,000,000,240,127,000,240,127,000,240,127,000,240,127,000,240,127,000,000,000)
+Get-ADUser jgomez | Set-ADUser -replace @{logonhours = $horario}
+
+
+#Asignar a Usuarios de OU
+[byte[]]$horario=@(000,000,000,000,240,127,000,240,127,000,240,127,000,240,127,000,240,127,000,000,000)
+Get-ADUser -Filter * -SearchBase "OU=Usuarios,DC=CONTOSO,DC=local" | Set-ADUser -replace @{logonhours = $horario}
